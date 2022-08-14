861a7585236bceec3b0353f76d1dd40cecc2e60b
rezwan2525 <rezwan2525@gmail.com>
2022-08-14 11:10:06 +0600

    added unit test

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import User, Post
 
-# Create your tests here.
+class BlogTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        #create user
+        test_user1 = User.objects.create_user(
+            username = 'testuser1',
+            password = 'testpass',
+        )
+        test_user1.save()
+        #create blogpost
+        test_post = Post.objects.create(
+            author = test_user1,
+            title = 'First Blog',
+            body = 'Blog Details Here',
+        )
+        test_post.save()
+
+    def test_blog_content(self):
+        post = Post.objects.get(id=1)
+        author = f'{post.author}'
+        title = f'{post.title}'
+        body = f'{post.body}'
+
+        self.assertEqual(author, 'testuser1')
+        self.assertEqual(title, 'First Blog')
+        self.assertEqual(body, 'Blog Details Here')
