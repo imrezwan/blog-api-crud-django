bb3d0008deea50032bbab9428f699dbaeaacc5c5
rezwan2525 <rezwan2525@gmail.com>
2022-08-15 14:04:52 +0600

    added permission in api endpoints

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+
+from .permissions import IsAuthorOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-
-
-class PostList(generics.ListAPIView):
+class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
